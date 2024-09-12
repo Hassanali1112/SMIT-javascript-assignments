ec062548c54b16f297387139e7557987ead4661a
hassan ali <hassanali0111272@gmail.com>
2024-09-12 22:42:19 +0500

    assignmem

---
+++ 02-variable & numbers/chapter 05/index.js
@@ -0,0 +1,97 @@
+// task 01
+// var x = parseInt(prompt("enter your first number",));
+// var y = parseInt(prompt("enter your second number"));
+// var z = x + y;
+// document.write("sum of "+ x +" and "+ y + " is " +z)
+
+// task 02
+// part (1)
+// var x = parseInt(prompt("enter your first number"));
+// var y = parseInt(prompt("enter your second number"));
+// var z = x - y;
+// document.write("subtraction of " + x + " and " + y + " is " + z);
+
+// part (2)
+// var x = parseInt(prompt("enter your first number"));
+// var y = parseInt(prompt("enter your second number"));
+// var z = x * y;
+// document.write("Multiplication of " + x + " and " + y + " is " + z);
+
+// part (3)
+// var x = parseInt(prompt("enter your first number"));
+// var y = parseInt(prompt("enter your second number"));
+// var z = x / y;
+// document.write("division of " + x + " and " + y + " is " + z);
+
+// part (4)
+// var x = parseInt(prompt("enter your first number"));
+// var y = parseInt(prompt("enter your second number"));
+// var z = x % y ;
+// document.write("Modulus of " + x + " and " + y + " is " + z);
+
+// task 3
+// var myVariable;
+// document.write(myVariable+"\n");
+// myVariable = 5;
+// myVariable = myVariable+1;
+// document.write(myVariable + "\n");
+// myVariable = myVariable + 7;
+// document.write(myVariable + "\n");
+// myVariable = myVariable - 1;
+// document.write(myVariable + "\n");
+// myVariable = myVariable % 3;
+// document.write(myVariable + "\n");
+ 
+// task 04
+// var ticketCost = 600;
+// var fiveTicketCost = ticketCost * 5;
+// document.write("one movie ticket cost is "+ ticketCost+"\nfive movie ticket cost is "+ fiveTicketCost );
+
+// task 05
+// var TableeNumber = prompt("Enter a number for table to be displayed");
+// var sum;
+// sum = TableeNumber *1
+// document.write(TableeNumber + " X 1 = " + sum +",")
+// sum = TableeNumber *2
+// document.write("\n" + TableeNumber + " X 2 = " + sum + ",");
+// sum = TableeNumber * 3
+// document.write("\n" + TableeNumber + " X 3 = " + sum + ",");
+// sum = TableeNumber * 4
+// document.write("\n" + TableeNumber + " X 4 = " + sum + ",");
+// sum = TableeNumber * 5
+// document.write("\n" + TableeNumber + " X 5 = " + sum + ",");
+// sum = TableeNumber * 6
+// document.write("\n" + TableeNumber + " X 6 = " + sum + ",");
+// sum = TableeNumber * 7
+// document.write("\n" + TableeNumber + " X 7 = " + sum + ",");
+// sum = TableeNumber * 8
+// document.write("\n" + TableeNumber + " X 8 = " + sum + ",");
+// sum = TableeNumber * 9
+// document.write("\n" + TableeNumber + " X 9 = " + sum + ",");
+// sum = TableeNumber * 10
+// document.write("\n" + TableeNumber + " X 10 = " + sum + ",");
+
+// task 6
+// part (1)
+// var temperatureInCelcius = prompt("Enter temperature in celcius");
+// var temperatureInFahrenheit = (temperatureInCelcius * 9 / 5) + 32;
+// document.write("the converted temperature in Fahrenheit is " + temperatureInFahrenheit);
+ 
+// part (2)
+// var  temperatureInFahrenheit = prompt("Enter temperature in Fahrenheit");
+// var temperatureInCelcius = (temperatureInFahrenheit - 32 )  * 5 / 9 ;
+// document.write("the converted temperature in celcius is " + temperatureInCelcius);
+
+// task 07
+var item1 = prompt("Enter price of item one in PKR");
+var quantityOFItem1 = prompt("Enter quantity of item one in numbers");
+var totalCostOfItem1 = item1 * quantityOFItem1;
+document.write("total cost of item one " + totalCostOfItem1 + "  in PKR." )
+
+var item2 = prompt("Enter price of item two in PKR");
+var quantityOFItem2 = prompt("Enter quantity of item two in numbers");
+ var totalCostOfItem2 = item2 * quantityOFItem2;
+document.write("total cost of item two is " + totalCostOfItem2 + " in PKR." )
+
+
+
