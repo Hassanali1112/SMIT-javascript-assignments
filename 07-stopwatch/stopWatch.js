45906adb0744f5d82ad177904c9d2f5d6710073d
hassan ali <hassanali0111272@gmail.com>
2024-10-27 11:02:28 -0700

    layout done

---
+++ 07-stopwatch/stopWatch.js
@@ -0,0 +1,8 @@
+const start = document.querySelector(".start");
+const stop = document.querySelector(".stop");
+const reset = document.querySelector(".reset");
+start.addEventListener("click",function (){
+alert("clicked")
+})
+// start.addEventListener("click",function (){})
+// start.addEventListener("click",function (){})
