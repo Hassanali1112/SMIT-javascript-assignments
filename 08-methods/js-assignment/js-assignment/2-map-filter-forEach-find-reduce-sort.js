e4f7e8ff846202c9fc925334d2a94b7d3bf6b5ba
hassan ali <hassanali0111272@gmail.com>
2024-11-12 18:59:50 +0500

    file2 task 2 done

--- 08-methods/js-assignment/js-assignment/2-map-filter-forEach-find-reduce-sort.js
+++ 08-methods/js-assignment/js-assignment/2-map-filter-forEach-find-reduce-sort.js
@@ -5,10 +5,11 @@ let marks = [90, 80, 65, 40, 75, 30, 25, 15];
 
 // 1- Print all the numbers greater then 40 in asc order using map
 let marksMoreThan40 = marks.filter( mark => mark > 40)
-console.log(marksMoreThan40)
+// console.log(marksMoreThan40)
 
 // 2- Find all the students having number equal to 70. 
-
+let marksEqual70 = marks.filter(mark => mark === 70 )
+console.log(marksEqual70)
 
 // 3- Find all the numbers less then 40 using filter function.
 
