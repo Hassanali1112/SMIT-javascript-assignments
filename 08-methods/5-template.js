c21bb499e24ec779abb969a57aecaa8b720a1282
hassan ali <hassanali0111272@gmail.com>
2024-11-17 19:06:37 +0500

    all done

--- 08-methods/5-template.js
+++ 08-methods/5-template.js
@@ -1,12 +1,16 @@
 //                    Assignment
 let x = 10;
 let y = 20;
+let sum = x + y;
 // 1- Print using template strings (sum of 10 + 20 = 30). 
+console.log(`The sum of ${x} + ${y} = ${sum}`);
 
 
 let product = [];
 // 2- How to test an empty array 
-
+if(product.length === 0){
+    console.log(`product is an empty array`)
+}
 
 
 // Declare an object with shorthand syntax based on followign three properties
@@ -14,5 +18,9 @@ let cat = 'Miaow';
 let dog = 'Woof';
 let bird = 'Peet peet';
 
-
+let newObj = new Object();
+newObj.cat = 'Miaow';
+newObj.dog = 'Woof';
+newObj.bird = 'Peet peet';
+console.log(newObj)
 
